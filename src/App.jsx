0332890ac239f3d68d99f2b95c27d979b0bf846e
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-01 05:52:04 +0200

    buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
+import * as qstr from './qstr';
+import styled from 'styled-components';
 
 const url = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
 
 function App() {
 	const [nouns, setNouns] = useState([]);
+	const [choice, setChoice] = useState('hide');
 
 	useEffect(() => {
 		(async () => {
-			setNouns((await axios.get(url)).data);
+			const _nouns = (await axios.get(url)).data;
+			qstr.randomize(_nouns);
+			setNouns(_nouns);
 		})();
 	}, []);
+
 	return (
 		<div className="App">
 			<h1>German Article Practice</h1>
-			<div>There are {nouns.length} nouns.</div>	
+
+			<div className="buttons">
+				<button onClick={() => setChoice('der')}>der</button>
+				<button onClick={() => setChoice('die')}>die</button>
+				<button onClick={() => setChoice('das')}>das</button>
+				<button onClick={() => setChoice('hide')}>hide</button>
+			</div>
+
+			<div className="nouns">
+				{nouns.map((noun, index) => {
+					return (
+						<div className="noun">
+							{noun.article} {noun.singular}
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
