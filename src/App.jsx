10015d564c09f866b12da23d63b2709bf71daf7e
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-01 05:27:09 +0200

    loading API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.scss'
+import { useState, useEffect } from 'react';
+import './App.scss';
+import axios from 'axios';
+
+const url = 'https://edwardtanguay.netlify.app/share/germanNouns.json';
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Info Site</h1>
-		<p>Welcome to this site.</p>
-    </div>
-  )
+	const [nouns, setNouns] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			setNouns((await axios.get(url)).data);
+		})();
+	}, []);
+	return (
+		<div className="App">
+			<h1>German Article Practice</h1>
+			<div>There are {nouns.length} nouns.</div>	
+		</div>
+	);
 }
 
 export default App;
